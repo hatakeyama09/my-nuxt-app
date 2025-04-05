46bc19c981d3da465ecd2adf0cb5f50d39652e1b
hatakeyama <tianshanguangren4@gmail.com>
2025-04-05 11:37:08 +0900

    first commit

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="header">
+        <h1>自己紹介サイト</h1>
+    </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="css">
+    .header {
+        background-color: EDF9FA;
+        display: flex;
+    }
+</style>
