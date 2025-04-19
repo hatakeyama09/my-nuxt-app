b66b3744430a32d77472c6285bf5355e282c3b85
hatakeyama <tianshanguangren4@gmail.com>
2025-04-19 11:34:26 +0900

    wip ヘッダーを新規作成（未完成） ハイドレーションエラーの調査

--- app.vue
+++ app.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import Header from "~/layouts/header.vue";
 </script>
 
 <template>
   <div>
     <NuxtLayout>
+      <Header />
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="css">
+body {
+  margin: 0 !important; 
+  padding: 0;
+}
+</style>
---
+++ layouts/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="self-introduction-header">
+    <div class="header-contents">
+      <h1>自己紹介</h1>
+      <div class="menu-item">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/blog">Blogs</NuxtLink>
+        <NuxtLink to="/repos">Repos</NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.self-introduction-header {
+  background-color: #edf9fa;
+}
+
+.header-contents {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.menu-item {
+  display: flex;
+  gap: 20px;
+}
+</style>
